31022cd0da5b1ed701e75524427d6a75e2ef5882
taha <stmh1363@gmail.com>
2025-11-09 21:15:25 +0330

    changed student status to enum

---
+++ SchoolApi/Domain/Enums/StudentStatus.cs
@@ -0,0 +1,15 @@
+namespace SchoolApi.Domain.Enums
+{
+    public enum StudentStatus
+    {
+        Active = 1,
+        Graduated = 2,
+        Suspended = 3,
+        Transferred = 4,
+        Inactive = 5
+    }
+}
+
+
+
+
