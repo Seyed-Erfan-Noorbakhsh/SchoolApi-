9fd283b30f1965541a796980f5159a164d280be5
taha <stmh1363@gmail.com>
2025-11-14 15:22:22 +0330

    added middleware

--- SchoolApi/Program.cs
+++ SchoolApi/Program.cs
@@ -6,6 +6,7 @@ using SchoolApi.Application.Services;
 using SchoolApi.Domain.Interfaces;
 using SchoolApi.Infrastructure.Data;
 using SchoolApi.Infrastructure.Repositories;
+using SchoolApi.Middleware;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -68,11 +69,10 @@ using (var scope = app.Services.CreateScope())
     context.Database.Migrate();
 }
 
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseSwagger();
 app.UseSwaggerUI();
-
-
-// Middlewares
 
 app.UseAuthentication();    
 app.UseAuthorization();     
